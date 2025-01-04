38a23a624420c1f0d0c5135ce86e9c711fcc85be
dodms <dodms1006@naver.com>
2025-01-04 20:09:08 +0900

    로그인 기능 추가

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser')
+const {auth} = require('./middleware/auth')
 const {User} = require('./models/User')
 const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI)
@@ -19,7 +21,7 @@ app.get('/', (req, res) => {
   res.send('Hello World! :)')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
     user.save().then(()=> {
         res.status(200).json({success:true})
@@ -27,6 +29,35 @@ app.post('/register', (req, res) => {
         return res.json({success:false, err})
     })
 })
+
+app.post('/api/users/login', (req, res) => {
+  //req로 넘어온 이메일이 데이터베이스에 있는지 확인
+  User.findOne({email: req.body.email})
+  .then(user => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false, 
+        message: "no match"
+      })
+    }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      if(!isMatch) return res.json({loginSuccess: false, message: "wrong password"})
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+      
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+      })
+    })    
+  })
+  .catch((err) => {
+    return res.status(400).send(err)
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
         type: String, 
         maxlength: 50
     },
-    emal:{
+    email:{
         type: String,
         trim: true, // 스페이스를 없애주는 역할
         unique: 1
@@ -30,6 +33,43 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return nect(err)
+    
+                user.password = hash
+                next()
+            })
+        })
+    } else{
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save().then(() => {
+        cb(null, user)
+    }).catch((err) => {
+        return cb(err)
+    })
+}
 
 const User = mongoose.model("User", userSchema)
 
